96b229609c2cdb6f3c2d182168d4bd04e42412eb
skwh <somekidwithhtml@gmail.com>
2012-11-18 15:32:20 -0700

    Improved user friendliness

--- src/net/skwh/NationsAndRanks/Listeners/PlayerInteractListener.java
+++ src/net/skwh/NationsAndRanks/Listeners/PlayerInteractListener.java
@@ -3,6 +3,7 @@ package net.skwh.NationsAndRanks.Listeners;
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Nation;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
@@ -17,11 +18,19 @@ public class PlayerInteractListener extends Core implements Listener {
 				Sign s = (Sign) b;
 				String signLineTwo = s.getLine(2);
 				if (getBaseCore().getNationNames().contains(signLineTwo)) {
+					boolean failed = false;
 					try {
 						Nation n = getBaseCore().findNationByName(signLineTwo);
 						n.addCitizen(e.getPlayer());
 					} catch (Exception ex) {
+						failed = true;
 						getBaseCore().log("There was a problem adding player " + e.getPlayer().getDisplayName() + " to a nation: " + ex.getMessage());
+					} finally {
+						if (failed) {
+							e.getPlayer().sendMessage(ChatColor.RED + "Sorry, there was a problem adding you to this nation.");
+						} else {
+							e.getPlayer().sendMessage(ChatColor.GREEN + "Welcome to this nation," + e.getPlayer().getDisplayName());
+						}
 					}
 				}
 			}
