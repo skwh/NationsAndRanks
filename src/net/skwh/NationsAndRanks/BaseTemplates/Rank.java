5eaaa293f5f582edc63863042baee8e66f64433e
skwh <somekidwithhtml@gmail.com>
2012-11-18 15:30:38 -0700

    Improved Rank class based on specifications

--- src/net/skwh/NationsAndRanks/BaseTemplates/Rank.java
+++ src/net/skwh/NationsAndRanks/BaseTemplates/Rank.java
@@ -1,19 +1,36 @@
 package net.skwh.NationsAndRanks.BaseTemplates;
 
+import org.bukkit.inventory.Inventory;
+
 public class Rank {
-	public String name;
+	public final String name;
 	private Rank up;
 	private Rank down;
+	private Inventory kit;
+	private final int payRequired;
+	
 	public Rank getUp() {
-		return this.up;
+		return up;
 	}
 	public Rank getDown() {
-		return this.down;
+		return down;
 	}
 	
-	public Rank(Rank u,Rank d,String n) {
+	public int getPayRequired() {
+		return payRequired;
+	}
+	
+	public Inventory getKit() {
+		return kit;
+	}
+	public void setKit(Inventory kit) {
+		this.kit = kit;
+	}
+	
+	public Rank(Rank u,Rank d,String n,int pR) {
 		name = n;
 		up = u;
 		down = d;
+		payRequired = pR;
 	}
 }
