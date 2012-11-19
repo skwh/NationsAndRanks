b24d702cc1e01fe7e3e2b2ed97613242d14f3a9c
skwh <somekidwithhtml@gmail.com>
2012-11-19 03:27:03 -0700

    Added rest of loadKeys and assignLists assigns the aquired Objects to the master lists

--- src/net/skwh/NationsAndRanks/Config/Settings.java
+++ src/net/skwh/NationsAndRanks/Config/Settings.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.bukkit.inventory.ItemStack;
+
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Guild;
 import net.skwh.NationsAndRanks.BaseTemplates.Nation;
@@ -30,11 +32,45 @@ public class Settings extends ConfigLoader {
 	}
 	
 	protected void loadKeys() {
-		List<String> l1 = (List<String>) config.getList("Nations");
+		List<String> l1 = (List<String>) config.getList("Nations"); //get nations
 		basePlugin.log(l1.toString());
-		for (int i=0;i<l1.size();i++) {
+		for (int i=0;i<l1.size();i++) { //create each nation object
 			NationList.add(new Nation((String) l1.toArray()[i]));
 		}
-		
+		for (int i=0;i<NationList.size();i++) { //for each nation
+			String CurrentNationName = ((Nation)NationList.toArray()[i]).getName();
+			List<String> l2 = (List<String>) config.getList( CurrentNationName + ".Guilds"); //find the guilds
+			for (int j=0;j<l2.size();j++) { //for each guild
+				GuildList.add(new Guild((String)l2.toArray()[j],(Nation)NationList.toArray()[j])); //create the guild object
+				String CurrentGuildName = ((Guild) GuildList.toArray()[j]).getName();
+				List<String> l3 = (List<String>) config.getList(CurrentNationName + "." + CurrentGuildName + ".Ranks"); //find the ranks
+				for (int k=0;k<l3.size();j++) { //for each rank
+					RankList.add(new Rank((String) l3.toArray()[k],(Guild)GuildList.toArray()[k])); //create the rank object
+					String CurrentRankName = ((Rank) RankList.toArray()[k]).getName();
+					
+					int price = config.getInt(CurrentNationName + "." + CurrentGuildName + "." + CurrentRankName + ".Price");
+					List<Integer> items = (List<Integer>) config.getList(CurrentNationName + "." + CurrentGuildName + "." + CurrentRankName + ".Items");
+					((Rank) RankList.toArray()[k]).setPayRequired(price);
+					for (int x=0;x<items.size();x++) { //for each item
+						((Rank) RankList.toArray()[k]).addToKit(new ItemStack((Integer) items.toArray()[x])); //create a new itemstack with that value
+					}
+				}
+				((Guild)GuildList.toArray()[j]).addRank((Rank)RankList.toArray()[j]);
+			}
+			try {
+				((Nation)NationList.toArray()[i]).addGuild((Guild)GuildList.toArray()[i]);
+			} catch (Exception e) {
+				basePlugin.log("There was a problem setting guilds: " + e.getMessage());
+			}
+		}
+		assignLists();
+	}
+	private void assignLists() {
+		for (int i=0;i<NationList.size();i++) {
+			Nation nationAtIndex = (Nation) NationList.toArray()[i];
+			basePlugin.getNationList().add((Nation) NationList.toArray()[i]);
+			basePlugin.getNation_NameList().put(nationAtIndex.getName(),nationAtIndex);
+			basePlugin.getNationNames().add(nationAtIndex.getName());
+		}
 	}
 }
