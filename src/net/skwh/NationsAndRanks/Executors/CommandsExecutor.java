63c9f622e4fedc59d3b692ae6028a3b7c3257c46
skwh <somekidwithhtml@gmail.com>
2012-11-20 10:15:04 -0700

    Changed some methods to use JamesBond

--- src/net/skwh/NationsAndRanks/Executors/CommandsExecutor.java
+++ src/net/skwh/NationsAndRanks/Executors/CommandsExecutor.java
@@ -2,6 +2,7 @@ package net.skwh.NationsAndRanks.Executors;
 
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Nation;
+import net.skwh.NationsAndRanks.Utilites.JamesBond;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,10 +10,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandsExecutor extends Core {
+	private static final JamesBond DoubleOhSeven = new JamesBond();
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length == 0) {
 			if (cmd.getName().equalsIgnoreCase("nar") || cmd.getName().equalsIgnoreCase("NationsAndRanks")) {
-				sender.sendMessage(ChatColor.GREEN + "NationsAndRanks v" + baseCore.getVersion() + " by SKWH");
+				sender.sendMessage(ChatColor.GREEN + "NationsAndRanks v" + getBaseCore().getVersion() + " by SKWH");
 			}
 		}
 		if (!(sender instanceof Player)) {
@@ -20,7 +22,7 @@ public class CommandsExecutor extends Core {
 			return true;
 		}
 		Player playah = (Player) sender;
-		if (args.length == 1) { //TODO: Add listener for JoinGuild
+		if (args.length == 1) { //TODO: finish listener for JoinGuild
 			if (cmd.getName().equalsIgnoreCase("SetVerbose") || cmd.getName().equalsIgnoreCase("sv")) {
 				boolean set;
 				if (args[0] == "true") {
@@ -31,18 +33,55 @@ public class CommandsExecutor extends Core {
 					sender.sendMessage(ChatColor.RED + "Please enter true or false.");
 					return true;
 				}
-				baseCore.setVerbosity(set);
+				getBaseCore().setVerbosity(set);
 			}
 			//chat command to join a nation
 			if (cmd.getName().equalsIgnoreCase("JoinNation") || cmd.getName().equalsIgnoreCase("jn") || cmd.getName().equalsIgnoreCase("join")) {
-				boolean nationExists = baseCore.getNation_NameList().containsKey(args[0]);
-				if (nationExists) {
-					Nation n = baseCore.getNation_NameList().get(args[0]);
-					try {
-						n.addCitizen(playah);
-					} catch (Exception e) {
-						baseCore.log("Error adding player to nation: " + e.getMessage());
+				if (!DoubleOhSeven.doesPlayerBelongToCountry(playah)) {
+					boolean nationExists = baseCore.getNation_NameList().containsKey(args[0]);
+					if (nationExists) {
+						Nation n = getBaseCore().getNation_NameList().get(args[0]);
+						boolean failure = false;
+						try {
+							n.addCitizen(playah);
+						} catch (Exception e) {
+							failure = true;
+							getBaseCore().log("Error adding player to nation: " + e.getMessage());
+						} finally {
+							if (failure) {
+								playah.sendMessage(ChatColor.RED + "Sorry, there was a problem adding you to " + args[0] + ".");
+							} else {
+								playah.sendMessage(ChatColor.GREEN + "Congradulations, Welcome to " + args[0] + ", " + playah.getDisplayName());
+								n.refreshCitizens();
+							}
+						}
 					}
+				} else {
+					playah.sendMessage("You are already a citizen of a nation!");
+				}
+			}
+			if (cmd.getName().equalsIgnoreCase("JoinGuild") || cmd.getName().equalsIgnoreCase("jg")) {
+				if (DoubleOhSeven.doesPlayerBelongToCountry(playah)) {
+					if (!DoubleOhSeven.doesPlayerBelongToGuild(playah)) {
+						boolean failed = false;
+						try {
+							Nation n = getBaseCore().findNationByName(args[0]);
+							n.addCitizen(playah);
+						} catch (Exception e) {
+							failed = true;
+							getBaseCore().log("There was a problem adding player " + playah.getDisplayName() + " to a guild: " + e.getMessage());
+						} finally {
+							if (failed) {
+								playah.sendMessage(ChatColor.RED + "Sorry, there was a problem adding you to this nation.");
+							} else {
+								playah.sendMessage(ChatColor.GREEN + "Welcome to this nation," + playah.getDisplayName() + "!");
+							}
+						}
+					} else {
+						playah.sendMessage("You are already in a guild!");
+					}
+				} else {
+					playah.sendMessage("You must belong to a nation to join a guild!");
 				}
 			}
 		}
