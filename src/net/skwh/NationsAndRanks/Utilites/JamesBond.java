49dde0a5ddd2aa988e41762da472625350437056
skwh <somekidwithhtml@gmail.com>
2012-11-22 16:20:07 -0700

    Made userlist compliant

--- src/net/skwh/NationsAndRanks/Utilites/JamesBond.java
+++ src/net/skwh/NationsAndRanks/Utilites/JamesBond.java
@@ -9,35 +9,46 @@ import org.bukkit.entity.Player;
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Guild;
 import net.skwh.NationsAndRanks.BaseTemplates.Nation;
+import net.skwh.NationsAndRanks.BaseTemplates.User;
 
 public class JamesBond extends Core {
 	public boolean doesPlayerBelongToCountry(Player p) {
-		int NationListSize = getBaseCore().getNationList().size();
-		for (int i=0;i<NationListSize;i++) {
-			Nation n = (Nation) getBaseCore().getNationList().toArray()[i];
-			for (int j=0;j<n.getCitizens().size();j++) {
-				if (p == n.getCitizens().toArray()[j]) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	public boolean doesPlayerBelongToGuild(Player p) {
-		int NationListSize = getBaseCore().getNationList().size();
-		for (int i=0;i<NationListSize;i++) {
-			Nation n = (Nation) getBaseCore().getNationList().toArray()[i];
-			for (int j=0;j<n.getGuilds().size();j++) {
-				Guild g = (Guild) n.getGuilds().toArray()[j];
-				for (int k=0;k<g.getMembers().size();k++) {
-					if (p == g.getMembers().toArray()[k]) {
+		if (!isPlayerInUserList(p)) {
+			int NationListSize = getBaseCore().getNationList().size();
+			for (int i=0;i<NationListSize;i++) {
+				Nation n = (Nation) getBaseCore().getNationList().toArray()[i];
+				for (int j=0;j<n.getCitizens().size();j++) {
+					if (p == n.getCitizens().toArray()[j]) {
 						return true;
 					}
 				}
 			}
+			return false;
+		} else {
+			User u = getBaseCore().getUserList().get(p);
+			return u.doesBelongToNation();
 		}
-		return false;
+	}
+	
+	public boolean doesPlayerBelongToGuild(Player p) {
+			if (!isPlayerInUserList(p)) {
+			int NationListSize = getBaseCore().getNationList().size();
+			for (int i=0;i<NationListSize;i++) {
+				Nation n = (Nation) getBaseCore().getNationList().toArray()[i];
+				for (int j=0;j<n.getGuilds().size();j++) {
+					Guild g = (Guild) n.getGuilds().toArray()[j];
+					for (int k=0;k<g.getMembers().size();k++) {
+						if (p == g.getMembers().toArray()[k]) {
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		} else {
+			User u = getBaseCore().getUserList().get(p);
+			return u.doesBelongToGuild();
+		}
 	}
 	
 	public Set<String> getGuildNames() {
@@ -65,5 +76,49 @@ public class JamesBond extends Core {
 			}
 		}
 		return GuildSet;
+	}
+	
+	public Guild getGuildForPlayer(Player p) {
+		if (isPlayerInUserList(p)) {
+			return getBaseCore().getUserList().get(p).getGuild();
+		} else {
+			HashMap<String,Guild> hashGuilds = getGuilds();
+			Set<String> GuildNames = getGuildNames();
+			for (int i=0;i<GuildNames.size();i++) {
+				Guild currentGuild = hashGuilds.get(GuildNames.toArray()[i]);
+				for (int a=0;a<currentGuild.getMembers().size();a++) {
+					if ((Player)currentGuild.getMembers().toArray()[a] == p) {
+						return currentGuild;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public Nation getNationForPlayer(Player p) {
+		if (isPlayerInUserList(p)) {
+			return getBaseCore().getUserList().get(p).getNation();
+		} else {
+			HashMap<String,Nation> hashNations = getBaseCore().getNation_NameList();
+			Set<String> NationNames = getBaseCore().getNationNames();
+			for (int i=0;i<NationNames.size();i++) {
+				Nation currentNation = hashNations.get(NationNames.toArray()[i]);
+				for (int a=0;a<currentNation.getCitizens().size();a++) {
+					if ((Player) currentNation.getCitizens().toArray()[a] == p) {
+						return currentNation;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	public boolean isPlayerInUserList(Player p) {
+		return getBaseCore().getUserList().containsKey(p);
+	}
+	
+	public boolean isUserinUserList(User u) {
+		return getBaseCore().getUserList().containsValue(u);
 	}
 }
