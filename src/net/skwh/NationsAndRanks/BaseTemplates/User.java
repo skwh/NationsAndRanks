6f16c4ba3ee9c447a55154d5a8ff69939da7dad6
skwh <somekidwithhtml@gmail.com>
2012-11-30 07:03:12 -0700

    Added JavaDoc comments.

--- src/net/skwh/NationsAndRanks/BaseTemplates/User.java
+++ src/net/skwh/NationsAndRanks/BaseTemplates/User.java
@@ -48,11 +48,29 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
-
+/**
+ * A class used to represent a player, with additional methods and properties. 
+ * @author Evan Derby <somekidwithhtml@gmail.com>
+ * @version 0.8
+ * @since 2012-11-29
+ */
 public class User implements Player {
+	/**
+	 * Adds the given user to the user list in the given {@link Core}.
+	 * @param basePlugin {@link Core}
+	 * @param u {@link User}
+	 */
 	public static void addToUserList(Core basePlugin, User u) {
 		basePlugin.getUserList().put(u.p, u);
 	}
+	/**
+	 * Creates a new user with the attributes given and returns it.
+	 * @param p {@link Player}
+	 * @param n {@link Nation}
+	 * @param g {@link Guild}
+	 * @param r {@link Rank}
+	 * @return {@link User}
+	 */
 	public static User makeUserWithAttributes(Player p,Nation n, Guild g, Rank r) {
 		if (p == null) {
 			return null;
@@ -70,49 +88,108 @@ public class User implements Player {
 		return u;
 	}
 	
+	/**
+	 * The Player that this user references.
+	 */
 	private Player p;
+	/**
+	 * Whether or not this user belongs to a {@link Nation}.
+	 */
 	private boolean belongsToNation = false;
+	/**
+	 * The {@link Nation} that this user might belong to.
+	 */
 	private Nation ownerNation = null;
 	
+	/**
+	 * Whether or not this user belongs to a {@link Guild}.
+	 */
 	private boolean belongsToGuild = false;
+	/**
+	 * The {@link Guild} that this player might belong to.
+	 */
 	private Guild ownerGuild = null;
 	
+	/**
+	 * The {@link Rank} that this player might have.
+	 */
 	private Rank currentRank = null;
 	
+	/**
+	 * Returns the {@link #currentRank}.
+	 * @return {@link Rank}
+	 */
 	public Rank getCurrentRank() {
 		return currentRank;
 	}
+	/**
+	 * Sets the current rank, and sets the player to that rank in the guild's player-rank list. ({@link Guild#getPlayerRankList()}).
+	 * @param r {@link Rank}
+	 */
 	public void setCurrentRank(Rank r) {
 		currentRank = r;
 		ownerGuild.setPlayerToRank(this.p,r);
 	}
 	
+	/**
+	 * Returns if the player belongs to a nation.
+	 * @return {@link Boolean}
+	 */
 	public boolean doesBelongToNation() {
 		return belongsToNation;
 	}
+	/**
+	 * Returns the nation object that this player might belong to.
+	 * @return {@link Nation}
+	 */
 	public Nation getNation() {
 		return ownerNation;
 	}
+	/**
+	 * Sets the nation that the player belongs to, and changes {@link #belongsToNation} correspondingly.
+	 * @param b {@link Boolean}
+	 * @param n {@link Nation}
+	 */
 	public void setNation(Boolean b,Nation n) {
 		belongsToNation = b;
 		ownerNation = n;
 	}
 	
+	/**
+	 * Returns if the player belongs to a guild.
+	 * @return {@link Boolean}
+	 */
 	public boolean doesBelongToGuild() {
 		return belongsToGuild;
 	}
+	/**
+	 * Returns the guild that the player might belong to.
+	 * @return {@link Guild}
+	 */
 	public Guild getGuild() {
 		return ownerGuild;
 	}
+	/**
+	 * Sets the guild that the player belongs to, and changes {@link #belongsToGuild} at the same time.
+	 * @param b {@link Boolean}
+	 * @param g {@link Guild}
+	 */
 	public void setGuild(Boolean b, Guild g) {
 		belongsToGuild = b;
 		ownerGuild = g;
 	}
 	
+	/**
+	 * Returns the player that this user references.
+	 */
 	public Player getPlayer() {
 		return p;
 	}
 	
+	/**
+	 * Basic constructor.
+	 * @param p {@link Player}
+	 */
 	public User(Player p) {
 		this.p = p;
 	}
--- src/net/skwh/NationsAndRanks/Utilites/JamesBond.java
+++ src/net/skwh/NationsAndRanks/Utilites/JamesBond.java
@@ -11,8 +11,19 @@ import net.skwh.NationsAndRanks.BaseTemplates.Guild;
 import net.skwh.NationsAndRanks.BaseTemplates.Nation;
 import net.skwh.NationsAndRanks.BaseTemplates.Rank;
 import net.skwh.NationsAndRanks.BaseTemplates.User;
-
+/**
+ * A class for finding information.
+ * @author Evan Derby <somekidwithhtml@gmail.com>
+ * @version 0.8
+ * @since 2012-11-29
+ */
 public class JamesBond extends Core {
+	
+	/**
+	 * Returns whether or not the given player belongs to a nation.
+	 * @param p {@link Player}
+	 * @return {@link Boolean}
+	 */
 	public boolean doesPlayerBelongToCountry(Player p) {
 		if (!isPlayerInUserList(p)) {
 			for (Nation n : getBaseCore().getNationList()) {
@@ -29,6 +40,11 @@ public class JamesBond extends Core {
 		}
 	}
 	
+	/**
+	 * Returns whether or not the given player belongs to a guild.
+	 * @param p {@link Player}
+	 * @return {@link Boolean}
+	 */
 	public boolean doesPlayerBelongToGuild(Player p) {
 			if (!isPlayerInUserList(p)) {
 			for (Nation n : getBaseCore().getNationList()) {
@@ -47,6 +63,10 @@ public class JamesBond extends Core {
 		}
 	}
 	
+	/**
+	 * Returns a set of all the guild names.
+	 * @return {@link Set}
+	 */
 	public Set<String> getGuildNames() {
 			Set<String> GuildNames = new HashSet<String>();
 			for (Nation n : getBaseCore().getNationList()) {
@@ -57,6 +77,10 @@ public class JamesBond extends Core {
 			return GuildNames;
 	}
 	
+	/**
+	 * Returns a hashmap of guild names mapped to their objects.
+	 * @return {@link HashMap}
+	 */
 	public HashMap<String,Guild> getGuilds() {
 		HashMap<String,Guild> GuildSet = new HashMap<String,Guild>();
 		for (Nation n : getBaseCore().getNationList()) {
@@ -67,6 +91,11 @@ public class JamesBond extends Core {
 		return GuildSet;
 	}
 	
+	/**
+	 * Returns the guild a player belongs to, if any.
+	 * @param p {@link Player}
+	 * @return {@link Guild}
+	 */
 	public Guild getGuildForPlayer(Player p) {
 		if (isPlayerInUserList(p)) {
 			return getBaseCore().getUserList().get(p).getGuild();
@@ -85,6 +114,11 @@ public class JamesBond extends Core {
 		return null;
 	}
 	
+	/**
+	 * Returns the nation a player belongs to, if any.
+	 * @param p {@link Player}
+	 * @return {@link Nation}
+	 */
 	public Nation getNationForPlayer(Player p) {
 		if (isPlayerInUserList(p)) {
 			return getBaseCore().getUserList().get(p).getNation();
@@ -103,6 +137,11 @@ public class JamesBond extends Core {
 		return null;
 	}
 	
+	/**
+	 * Extracts the names out of the given set of ranks and returns it as a set of strings.
+	 * @param arg0 {@link Set}&#60;{@link Rank}&#62;
+	 * @return names {@link Set}&#60;{@link String}&#62;
+	 */
 	public Set<String> rankGetNames(Set<Rank> arg0) {
 		Set<String> names = new HashSet<String>();
 		for (Rank r : arg0) {
@@ -111,6 +150,11 @@ public class JamesBond extends Core {
 		return names;
 	}
 	
+	/**
+	 * Extracts the names out of the given set of guilds and returns it as a set of strings.
+	 * @param arg0 {@link Set}&#60;{@link Guild}&#62;
+	 * @return names {@link Set}&#60;{@link String}&#62;
+	 */
 	public Set<String> guildGetNames(Set<Guild> arg0) {
 		Set<String> names = new HashSet<String>();
 		for (Guild g : arg0) {
@@ -119,6 +163,11 @@ public class JamesBond extends Core {
 		return names;
 	}
 	
+	/**
+	 * Extracts the names out of the given set of nations and returns it as a set of strings.
+	 * @param arg0 {@link Set}&#60;{@link Nation}&#62;
+	 * @return names {@link Set}&#60;{@link String}&#62;
+	 */
 	public Set<String> nationGetNames(Set<Nation> arg0) {
 		Set<String> names = new HashSet<String>();
 		for (Nation n : arg0) {
@@ -127,10 +176,20 @@ public class JamesBond extends Core {
 		return names;
 	}
 	
+	/**
+	 * Returns whether or not the given player is in the User list ({@link Core#getUserList()}).
+	 * @param p {@link Player}
+	 * @return {@link Boolean}
+	 */
 	public boolean isPlayerInUserList(Player p) {
 		return getBaseCore().getUserList().containsKey(p);
 	}
 	
+	/**
+	 * Returns whether or not the given user is in the User list ({@link Core#getUserList()}).
+	 * @param u {@link User}
+	 * @return {@link Boolean}
+	 */
 	public boolean isUserinUserList(User u) {
 		return getBaseCore().getUserList().containsValue(u);
 	}
