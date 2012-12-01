925367f853e0c5d061f36b6f3dfec21b3fc9ae4b
skwh <somekidwithhtml@gmail.com>
2012-12-01 00:59:04 -0700

    Moved the credits

--- src/net/skwh/NationsAndRanks/Utilites/SLAPI.java
+++ src/net/skwh/NationsAndRanks/Utilites/SLAPI.java
@@ -1,8 +1,3 @@
-/** SLAPI = Saving/Loading API
- * API for Saving and Loading Objects.
- * You can use this API in your projects, but please credit the original author of it.
- * @author Tomsik68<tomsik68@gmail.com>
- */
 package net.skwh.NationsAndRanks.Utilites;
 
 import java.io.FileInputStream;
@@ -10,6 +5,11 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/** SLAPI = Saving/Loading API
+ * API for Saving and Loading Objects.
+ * You can use this API in your projects, but please credit the original author of it.
+ * @author Tomsik68<tomsik68@gmail.com>
+ */
 public class SLAPI {
 	public static <T extends Object> void save(T obj,String path) throws Exception
 	{
