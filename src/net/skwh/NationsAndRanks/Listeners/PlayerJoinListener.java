73ae1b6b466abdbdcfe0da1a91ed79bb98a5e548
skwh <somekidwithhtml@gmail.com>
2012-12-01 02:14:18 -0700

    Changed from DoubleOhSeven to JamesBond because static

--- src/net/skwh/NationsAndRanks/Listeners/PlayerJoinListener.java
+++ src/net/skwh/NationsAndRanks/Listeners/PlayerJoinListener.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Guild;
+import net.skwh.NationsAndRanks.BaseTemplates.Nation;
 import net.skwh.NationsAndRanks.BaseTemplates.Rank;
 import net.skwh.NationsAndRanks.BaseTemplates.User;
 import net.skwh.NationsAndRanks.Utilites.JamesBond;
@@ -16,7 +17,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerJoinListener extends Core implements Listener {
-	private static final JamesBond DoubleOhSeven = new JamesBond();
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
@@ -24,23 +24,24 @@ public class PlayerJoinListener extends Core implements Listener {
 		String s = p.getName();
 		getBaseCore().log("Player " + p.getName() + " joined!");
 		getBaseCore().log("Has the player played before? " + p.hasPlayedBefore());
-		getBaseCore().log("Does the player belong to a country? " + DoubleOhSeven.doesPlayerBelongToCountry(s));
-		if (DoubleOhSeven.doesPlayerBelongToCountry(s)) {
-			DoubleOhSeven.getNationForPlayer(s).refreshCitizens();
+		getBaseCore().log("Does the player belong to a country? " + JamesBond.doesPlayerBelongToCountry(s));
+		if (JamesBond.doesPlayerBelongToCountry(s)) {
+			Nation n = JamesBond.getNationForPlayer(s);
+			n.refreshCitizens();
 		}
-		getBaseCore().log("Is the player in the user list? " + DoubleOhSeven.isPlayerInUserList(s));
-		if (!DoubleOhSeven.isPlayerInUserList(s)) {
+		getBaseCore().log("Is the player in the user list? " + JamesBond.isPlayerInUserList(s));
+		if (!JamesBond.isPlayerInUserList(s)) {
 			User u = new User(p);
 			getBaseCore().getUserList().put(s, u);
 			getBaseCore().log("A new user was added to the userlist, " + p.getName());
 		}
 		
-		getBaseCore().log("Does the player belong to a guild? " + DoubleOhSeven.doesPlayerBelongToGuild(s));
-		if (DoubleOhSeven.doesPlayerBelongToGuild(s)) {
+		getBaseCore().log("Does the player belong to a guild? " + JamesBond.doesPlayerBelongToGuild(s));
+		if (JamesBond.doesPlayerBelongToGuild(s)) {
 			boolean failed = false;
 			Set<ItemStack> i = new HashSet<ItemStack>();
 			try {
-				Guild g = DoubleOhSeven.getGuildForPlayer(s);
+				Guild g = JamesBond.getGuildForPlayer(s);
 				Rank r = g.getRankForPlayer(s);
 				i = r.getKit();
 			} catch (Exception ex) {
