2de2ed9c8b67f9c2c914928c9e6edd23b3cbd583
skwh <somekidwithhtml@gmail.com>
2012-12-01 23:34:10 -0700

    Removed setverbose, added getrank, restructured joinguild and joinnation, added displayMessageToPlayer and implemented it

--- src/net/skwh/NationsAndRanks/Executors/CommandsExecutor.java
+++ src/net/skwh/NationsAndRanks/Executors/CommandsExecutor.java
@@ -1,6 +1,7 @@
 package net.skwh.NationsAndRanks.Executors;
 
 import java.util.Set;
+import java.util.Vector;
 
 import net.skwh.NationsAndRanks.Core;
 import net.skwh.NationsAndRanks.BaseTemplates.Guild;
@@ -9,6 +10,7 @@ import net.skwh.NationsAndRanks.BaseTemplates.User;
 import net.skwh.NationsAndRanks.Utilites.JamesBond;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -36,24 +38,26 @@ public class CommandsExecutor extends Core {
 			if (cmd.getName().equalsIgnoreCase("list")) {
 				sender.sendMessage(ChatColor.DARK_PURPLE + cmd.getUsage());
 			}
+			if (cmd.getName().equalsIgnoreCase("getrank") || cmd.getName().equalsIgnoreCase("gr")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
+					return true;
+				}
+				User u = getBaseCore().getUserList().get(usahName);
+				if (u.doesBelongToGuild()) {
+					displayMessageToPlayer(playah, "Current Guild: " + u.getGuild().getName() + ", Current rank: " + u.getCurrentRank().getName());
+				} else if (u.doesBelongToNation()) {
+					displayMessageToPlayer(playah, "Current Nation: " + u.getNation().getName() + ", You do not belong to a guild yet.");
+				} else {
+					displayMessageToPlayer(playah, "You do not belong to a nation yet!");
+				}
+			}
 		}
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
 			return true;
 		}
 		if (args.length == 1) {
-			if (cmd.getName().equalsIgnoreCase("SetVerbose") || cmd.getName().equalsIgnoreCase("sv")) {
-				boolean set;
-				if (args[0].equalsIgnoreCase("true")) {
-					set = true;
-				} else if (args[0].equalsIgnoreCase("false")) {
-					set = false;
-				} else {
-					sender.sendMessage(ChatColor.RED + "Please enter true or false.");
-					return true;
-				}
-				getBaseCore().setVerbosity(set);
-			}
 			//chat command to join a nation
 			if (cmd.getName().equalsIgnoreCase("JoinNation") || cmd.getName().equalsIgnoreCase("jn")) {
 				if (!JamesBond.doesPlayerBelongToCountry(usahName)) {
@@ -63,28 +67,22 @@ public class CommandsExecutor extends Core {
 						boolean failure = false;
 						try {
 							n.addCitizen(usahName);
+							getBaseCore().getUserList().get(usahName).setNation(true, n);
 						} catch (Exception e) {
 							failure = true;
 							getBaseCore().log("Error adding player to nation: " + e.getMessage());
 						} finally {
 							if (failure) {
-								playah.sendMessage(ChatColor.RED + "Sorry, there was a problem adding you to " + args[0] + ".");
+								displayMessageToPlayer(playah, ChatColor.RED + "Sorry, there was a problem adding you to " + args[0] + ".");
 							} else {
-								playah.sendMessage(ChatColor.GREEN + "Congradulations, Welcome to " + args[0] + ", " + playah.getDisplayName());
-								n.refreshCitizens();
-								if (!JamesBond.isPlayerInUserList(usahName)) {
-									User u = User.makeUserWithAttributes(playah, n, null, null);
-									User.addToUserList(getBaseCore(), u);
-								} else {
-									getBaseCore().getUserList().get(usahName).setNation(true, n);
-								}
+								displayMessageToPlayer(playah, ChatColor.GREEN + "Welcome to " + args[0] + ", " + usahName);
 							}
 						}
 					} else {
-						playah.sendMessage("That nation does not exist!");
+						displayMessageToPlayer(playah, "That nation does not exist!");
 					}
 				} else {
-					playah.sendMessage("You are already a citizen of a nation!");
+					displayMessageToPlayer(playah, "You are already a citizen of a nation!");
 				}
 			}
 			//chat command to join a guild
@@ -96,67 +94,76 @@ public class CommandsExecutor extends Core {
 							Guild g = JamesBond.getGuilds().get(args[0]);
 							try {
 								g.addMemberWithRank(usahName,g.getRanksInOrder().elementAt(0));
+								getBaseCore().getUserList().get(usahName).setGuild(true, g);
 							} catch (Exception e) {
 								failed = true;
 								e.printStackTrace();
 							} finally {
 								if (failed) {
-									playah.sendMessage(ChatColor.RED + "Sorry, there was a problem adding you to " + args[0] + ".");
+									displayMessageToPlayer(playah, ChatColor.RED + "Sorry, there was a problem adding you to " + args[0] + ".");
 								} else {
-									playah.sendMessage(ChatColor.GREEN + "Welcome to " + args[0] + "," + playah.getDisplayName() + "!");
+									displayMessageToPlayer(playah, ChatColor.GREEN + "Welcome to " + args[0] + ", " + usahName);
 									JamesBond.getNationForPlayer(usahName).refreshCitizens();
-									if (!JamesBond.isPlayerInUserList(usahName)) {
-										User u = new User(playah);
-										u.setNation(true, JamesBond.getNationForPlayer(u.getUserName()));
-										u.setGuild(true, JamesBond.getGuildForPlayer(u.getUserName()));
-									} else {
-										getBaseCore().getUserList().get(usahName).setGuild(true, JamesBond.getGuildForPlayer(usahName));
-									}
 								}
 							}
 						} else {
-							playah.sendMessage("No guild with that name exists!");
+							displayMessageToPlayer(playah, "No guild with that name exists!");
 						}
 					} else {
-						playah.sendMessage("You are already in a guild!");
+						displayMessageToPlayer(playah, "You are already in a guild!");
 					}
 				} else {
-					playah.sendMessage("You must belong to a nation to join a guild!");
+					displayMessageToPlayer(playah, "You must belong to a nation to join a guild!");
 				}
 			}
 			//command to get the nations/guilds/ranks (list)
 			if (cmd.getName().equalsIgnoreCase("list")) {
 				if (args[0].equalsIgnoreCase("nations") || args[0].equalsIgnoreCase("nation")) {
-					playah.sendMessage("Nation list: " + getBaseCore().getNationNames().toString());
+					displayMessageToPlayer(playah, "Nation list: " + getBaseCore().getNationNames().toString());
 				} else if (args[0].equalsIgnoreCase("guilds") || args[0].equalsIgnoreCase("guild")) {
 					if (JamesBond.doesPlayerBelongToCountry(usahName)) {
 						Set<String> guildList = JamesBond.guildGetNames(JamesBond.getNationForPlayer(usahName).getGuilds());
-						playah.sendMessage("Guild list: " + guildList.toString());
+						displayMessageToPlayer(playah, "Guild list: " + guildList.toString());
 					} else {
-						playah.sendMessage(ChatColor.RED + "You must belong to a nation to get the list of guilds!");
+						displayMessageToPlayer(playah, ChatColor.RED + "You must belong to a nation to get the list of guilds!");
 					}
 				} else if (args[0].equalsIgnoreCase("ranks") || args[0].equalsIgnoreCase("rank")) {
 					if (JamesBond.doesPlayerBelongToGuild(usahName)) {
 						Guild g = JamesBond.getGuildForPlayer(usahName);
 						if (g == null) {
-							playah.sendMessage("There was an error getting the ranks for this guild.");
+							getBaseCore().log("There was an error getting the ranks for a player " + usahName);
+							displayMessageToPlayer(playah, "There was an error getting the ranks for this guild.");
 							return true;
 						}
-						Set<String> rankList = JamesBond.rankGetNames(g.getRanks());
-						playah.sendMessage("Rank List: " + rankList.toString());
+						Vector<String> rankList = JamesBond.rankGetNames(g.getRanksInOrder());
+						displayMessageToPlayer(playah, "Rank List: " + rankList.toString());
 					} else {
-						playah.sendMessage(ChatColor.RED + "You must belong to a guild to get the list of ranks!");
+						displayMessageToPlayer(playah, ChatColor.RED + "You must belong to a guild to get the list of ranks!");
 					}
 				}
 			}
-			// setguildspawn
+			// set guild spawn
 			if (cmd.getName().equalsIgnoreCase("SetGuildSpawn") || cmd.getName().equalsIgnoreCase("sgs")) {
-				
+				Location currentLocation = playah.getLocation();
+				if (JamesBond.getGuildNames().contains(args[0])) {
+					try {
+						JamesBond.getGuilds().get(args[0]).setSpawnPoint(currentLocation);
+					} catch (Exception e) {
+						e.printStackTrace();
+						displayMessageToPlayer(playah, ChatColor.RED + "Sorry, there was an error.");
+					}
+				}
 			}
 		}
-		if (args.length == 2) {
-			
-		}
 		return true;
+	}
+	
+	/**
+	 * Sends a given message to the given player.
+	 * @param p {@link Player}
+	 * @param m {@link String} Message
+	 */
+	private void displayMessageToPlayer(Player p, String m) {
+		p.sendMessage(m);
 	}
 }
